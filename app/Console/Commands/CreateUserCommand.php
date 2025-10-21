0d13deb86ce178a436692922298ea176a6ee4c63
Dmytro Shved <u_shved.dmytro@tdgjar.edu.pl>
2025-10-21 19:39:35 +0200

    refactor: create user command

---
+++ app/Console/Commands/CreateUserCommand.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class CreateUserCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     */
+    protected $signature = 'users:create';
+
+    /**
+     * The console command description.
+     */
+    protected $description = 'Creates a new user';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $user['name'] = $this->ask('Name of the new user');
+        $user['email'] = $this->ask('Email of the new user');
+        $user['password'] = $this->secret('Password of the new user');
+
+        $roleName = $this->choice('Role of the new user', ['admin', 'editor'], 1);
+        $role = Role::where('name', $roleName)->first();
+
+        if (! $role){
+            $this->error('Role not found');
+
+            return -1;
+        }
+
+        $validator = Validator::make($user, [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email','max:255', 'unique:users,email'],
+            'password' => ['required', 'min:8'],
+        ]);
+
+        if ($validator->fails()) {
+            foreach ($validator->errors()->all() as $error) {
+                $this->error($error);
+            }
+
+            return -1;
+        }
+
+        DB::transaction(function () use ($user, $role, $roleName){
+            $newUser = User::create($user);
+            $newUser->roles()->attach($role->id);
+
+            if ($roleName == 'admin'){
+                $editorRole = Role::where('name', 'editor')->first();
+
+                if (! $editorRole){
+                    $this->error('Role not found');
+
+                    return -1;
+                }
+
+                $newUser->roles()->attach($editorRole->id);
+            }
+
+            return 0;
+        });
+
+        $this->info('User '.$user['email'].' created successfully');
+
+        return 0;
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,44 +2,12 @@
 
 namespace Database\Seeders;
 
-use App\Models\Role;
-use App\Models\Travel;
-use App\Models\Tour;
-use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    public array $roles = [
-        'user',
-        'admin',
-        'editor',
-    ];
-
     public function run(): void
     {
-        foreach ($this->roles as $role){
-            Role::create([
-                'name' => $role,
-                'created_at' => now(),
-                'updated_at' => now(),
-            ]);
-        }
-
-        $users = User::factory(3)->create();
-
-        $userRoleUuid = Role::where('name', 'user')->value('id');
-        $adminRoleUuid = Role::where('name', 'admin')->value('id');
-        $editorRoleUuid = Role::where('name', 'editor')->value('id');
-
-        $users->take(2)->each(function ($user) use ($userRoleUuid){
-            $user->roles()->attach($userRoleUuid);
-        });
-
-        $adminUser = $users->last();
-        $adminUser->roles()->attach([$adminRoleUuid, $editorRoleUuid]);
-
-        Travel::factory(5)->create();
-        Tour::factory(5)->create();
+        $this->call(RoleSeeder::class);
     }
 }
