63b93f992820e18843c4d0166a02326a0a95cddc
Dmytro Shved <u_shved.dmytro@tdgjar.edu.pl>
2025-11-25 17:35:04 +0100

    chore: fetch auth user route

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,12 @@
 use App\Http\Controllers\Api\V1\Admin;
 use App\Http\Controllers\Api\V1\TourController;
 use App\Http\Controllers\Api\V1\TravelController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
 
 Route::get('travels', [TravelController::class, 'index']);
 
